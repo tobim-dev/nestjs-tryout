c6e7f8bc92987ff521da189c3593cb8d103922cf
tobim-dev <mail@tmeyer.me>
2021-05-14 16:32:14 +0200

    add config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,12 +6,15 @@ import { UserRepository } from './user.repository';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
+import * as config from 'config';
+
+const jwtConfig = config.get('jwt');
 
 @Module({
   imports: [
     JwtModule.register({
-      secret: 'topSecret51',
-      signOptions: { expiresIn: 3600 },
+      secret: process.env.JWT_SECRET || jwtConfig.secret,
+      signOptions: { expiresIn: jwtConfig.expiresIn },
     }),
     PassportModule.register({ defaultStrategy: 'jwt' }),
     TypeOrmModule.forFeature([UserRepository]),
